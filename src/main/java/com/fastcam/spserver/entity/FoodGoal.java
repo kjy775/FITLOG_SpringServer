0ec5e0830a842be0877c00eff364e98ee3e52805
soyoeng419 <kuemokim@gmail.com>
2026-08-18 16:26:12 +0900

    entity

---
+++ src/main/java/com/fastcam/spserver/entity/FoodGoal.java
@@ -0,0 +1,22 @@
+package com.fastcam.spserver.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class FoodGoal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int num;
+    @Column(nullable = false)
+    private int goalCalories;
+    private float goalCarbs;
+    private float goalProtein;
+    private float goalFat;
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "mnum")
+    private Member member;
+}
