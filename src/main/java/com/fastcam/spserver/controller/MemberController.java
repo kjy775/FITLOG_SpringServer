4c334a80e8af8fb656ea07ffe8a14cf93a5f9bfe
soyoeng419 <kuemokim@gmail.com>
2026-08-18 14:54:59 +0900

    수정

--- src/main/java/com/fastcam/spserver/controller/MemberController.java
+++ src/main/java/com/fastcam/spserver/controller/MemberController.java
@@ -1,45 +1,193 @@
 package com.fastcam.spserver.controller;
 
+import com.fastcam.spserver.dto.KakaoProfile;
+import com.fastcam.spserver.dto.OAuthToken;
 import com.fastcam.spserver.entity.Member;
 import com.fastcam.spserver.service.MemberService;
+import com.google.gson.Gson;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Calendar;
 import java.util.HashMap;
 
 @RestController
 @RequestMapping("/member")
-
+@CrossOrigin({"http://localhost:8000", "http://localhost:3000"})
 public class MemberController {
 
     @Autowired
     MemberService ms;
 
-    // 로그인
+    @PostMapping("/join")
+    public HashMap<String, Object> join(@RequestBody Member member){
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        ms.insertMember( member );
+        map.put("msg", "OK");
+        return map;
+    }
+
     @PostMapping("/loginLocal")
-    public HashMap<String, Object> loginLocal(
-            @RequestParam("id") String id,
-            @RequestParam("pass") String pass) {
-        HashMap<String, Object> map = new HashMap<>();
-        Member mdto = ms.getMember(id);
-        if (mdto == null)
-            map.put("msg", "아이디/패스워드를 확인하세요");
-        else if (!mdto.getPass().equals(pass))
-            map.put("msg", "아이디/패스워드를 확인하세요");
-        else {
+    public HashMap<String, Object> loginLocal(@RequestBody Member member){
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        Member mdto = ms.getMemberById( member.getId() );
+        if( mdto == null)
+            map.put("msg", "아이디 패스워드를 확인하세요");
+        else if( !mdto.getPass().equals( member.getPass() ) )
+            map.put("msg", "아이디 패스워드를 확인하세요");
+        else{
             map.put("msg", "OK");
             map.put("loginUser", mdto);
-            System.out.println(mdto);
         }
         return map;
     }
 
-    // 회원가입
-    @PostMapping("/join")
-    public HashMap<String, Object> join(@RequestBody  Member member){
+    @Autowired
+    ServletContext sc;
+
+    @PostMapping("/fileupload")
+    public HashMap<String, Object> fileupload(@RequestParam("image") MultipartFile file){
         HashMap<String, Object> map = new HashMap<String, Object>();
-        ms.insertMember(member);
+        String path = sc.getRealPath("/image");
+        Calendar today = Calendar.getInstance();
+        long dt = today.getTimeInMillis();
+        String filename = file.getOriginalFilename();
+        String f1 = filename.substring(0, filename.lastIndexOf("."));
+        String f2 = filename.substring(filename.lastIndexOf("."));
+        String uploadPath = path + "/" + f1 + dt + f2;
+        try {
+            file.transferTo( new File(uploadPath) );
+            map.put("filename", f1 + dt + f2);
+        } catch (IllegalStateException | IOException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+
+    @PostMapping("/idcheck")
+    public HashMap<String, Object> idcheck(@RequestParam("id") String id) {
+        HashMap<String, Object> map = new HashMap<>();
+        Member mdto = ms.getMemberById(id);
+        if(mdto == null)
+            map.put("msg", "OK");
+        else
+            map.put("msg", "notOK");
+        return map;
+    }
+
+    @Value("${kakao.client_id}")
+    private String client_id;
+
+    @Value("${kakao.redirect_uri}")
+    private String redirect_uri;
+
+    @GetMapping("/kakaostart")
+    public @ResponseBody String kakaostart() {
+        String a = "<script type='text/javascript'>"
+                + "location.href='https://kauth.kakao.com/oauth/authorize?"
+                + "client_id=" + client_id
+                + "&redirect_uri=" + redirect_uri
+                + "&response_type=code'" + "</script>";
+        return a;
+    }
+
+    @GetMapping("/kakaoLogin")
+    public void kakaoLogin(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String code = request.getParameter("code");
+        String endpoint = "https://kauth.kakao.com/oauth/token";
+        URL url = new URL(endpoint);
+        String bodyData = "grant_type=authorization_code&";
+        bodyData += "client_id=" + client_id + "&";
+        bodyData += "redirect_uri=" + redirect_uri + "&";
+        bodyData += "code=" + code;
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+        conn.setDoOutput(true);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream(), "UTF-8"));
+        bw.write(bodyData);
+        bw.flush();
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+        String input = "";
+        StringBuilder sb = new StringBuilder();
+        while ((input = br.readLine()) != null) {
+            sb.append(input);
+        }
+        Gson gson = new Gson();
+        OAuthToken oAuthToken = gson.fromJson(sb.toString(), OAuthToken.class);
+        String endpoint2 = "https://kapi.kakao.com/v2/user/me";
+        URL url2 = new URL(endpoint2);
+
+        HttpsURLConnection conn2 = (HttpsURLConnection) url2.openConnection();
+        conn2.setRequestProperty("Authorization", "Bearer " + oAuthToken.getAccess_token());
+        conn2.setDoOutput(true);
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(conn2.getInputStream(), "UTF-8"));
+        String input2 = "";
+        StringBuilder sb2 = new StringBuilder();
+        while ((input2 = br2.readLine()) != null) {
+            sb2.append(input2);
+            //System.out.println(input2);
+        }
+
+        Gson gson2 = new Gson();
+        KakaoProfile kakaoProfile = gson2.fromJson(sb2.toString(), KakaoProfile.class);
+        KakaoProfile.KakaoAccount ac = kakaoProfile.getAccount();
+        KakaoProfile.KakaoAccount.Profile pf = ac.getProfile();
+
+        System.out.println("id : " + kakaoProfile.getId());
+        System.out.println("Profile-Nickname : " + ac.getProfile().getNickname());
+        System.out.println("Profile-pfimg : " + pf.getProfile_image_url());
+
+        Member mdto = ms.getMemberById(kakaoProfile.getId());
+        if (mdto == null) {
+            mdto = new Member();
+
+            mdto.setId(kakaoProfile.getId());
+            //mdto.setSnsid(kakaoProfile.getId());
+            mdto.setName(ac.getProfile().getNickname());
+            mdto.setProfileImg(pf.getProfile_image_url());
+            mdto.setProvider("KAKAO");
+
+            ms.insertMember(mdto);
+            mdto = ms.getMemberById(kakaoProfile.getId());
+        }
+        response.sendRedirect("http://localhost:3000/savekakaoinfo/" + mdto.getNum());
+    }
+
+    @PostMapping("/kakaoinfoUpdate")
+    public HashMap<String, Object> kakaoinfoUpdate(@RequestBody Member member){
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("msg", "OK");
+        map.put("loginUser", ms.getMemberById( member.getId() ) );
+        return map;
+    }
+
+    @PostMapping("/updateMember")
+    public HashMap<String, Object> updateMember(@RequestBody Member member) {
+        HashMap<String, Object> map = new HashMap<>();
+        ms.updateMember(member);
         map.put("msg", "OK");
         return map;
     }
+
+    @DeleteMapping("/deleteMember")
+    public HashMap<String, Object> deleteMember(@RequestParam("id") String id) {
+        HashMap<String, Object> map = new HashMap<>();
+        ms.deleteMember(id);
+        map.put("msg", "OK");
+        return map;
+    }
+
 }
+
